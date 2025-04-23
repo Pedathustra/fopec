0dad501473c84455225a7b31d667796c9c408ccb
ped <lp@pedathustra.com>
2025-04-23 00:58:05 +0000

    chore: new proc

---
+++ database/fopec_database/fopec_database/dbo.getCrowdsourcedResearchByPersonId.sql
@@ -0,0 +1,35 @@
+if exists(select 1 from sys.procedures where name = 'getCrowdsourcedResearchByPersonId')
+begin 
+	drop proc getCrowdsourcedResearchByPersonId
+end
+go
+
+create proc getCrowdsourcedResearchByPersonId
+	@person_id int
+as
+select	cr.id crowdsourced_id
+	,	c.id as company_id
+	,	c.name as company_name
+	,	cr.parent_company_id
+	,	pc.name parent_company_name
+	,	ot.id as ownership_type_id
+	,	ot.description as ownership_type_description
+	,	p.username
+	,	cr.created
+	,	cr.notes	
+from crowdsourced_research cr 
+	join company c on cr.company_id = c.id
+	join ownership_type ot on cr.ownership_type_id = ot.id
+	join person p on cr.observing_person_id = p.id
+	left join company pc on cr.parent_company_id = pc.id
+where p.id = @person_id
+order by cr.created desc
+;
+go
+
+--select parent_company_id from crowdsourced_research
+--exec getCrowdsourcedResearchByPersonId 132
+
+--select * from person
+
+
