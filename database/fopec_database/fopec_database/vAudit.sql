e07d9703929ed46dea3baa0506b0f8fd33eb8a2e
ped <lp@pedathustra.com>
2025-04-26 19:03:27 +0000

    chore: db vAudit

---
+++ database/fopec_database/fopec_database/vAudit.sql
@@ -0,0 +1,61 @@
+if exists(select * from sys.views where name = 'vPersonAudit')
+begin
+	drop view vPersonAudit
+end
+go
+
+
+create view vPersonAudit
+as 
+   
+
+	select  t.id
+		,	t.first_name
+		,	t.last_name
+		,	t.middle_name
+		,	t.username 
+		,	t.action_date
+		,	t.is_active
+		,	t.record_type
+		,	idx = row_number()over(partition by t.id order by  action_date asc)
+	from (
+			select p.id
+				,	p.first_name
+				,	p.last_name
+				,	p.middle_name
+				,	p.username 
+				,	p.created action_date
+				,	p.is_active
+				,	'person' record_type
+			from person p
+			union 
+
+			select  pa.person_id
+				,	pa.first_name
+				,	pa.last_name
+				,	pa.middle_name
+				,	pa.username
+				,	pa.audit_datetime
+				,	pa.is_active
+				,	'audit' record_type
+			from person_audit pa
+	) t 
+
+ 
+go
+	/*
+	select  a.id,
+			a.first_name,
+			a.last_name,
+			a.middle_name,
+			a.username ,
+			a.action_date,
+			a.is_active,
+			a.record_type,
+			a.idx 
+	from vPersonAudit a
+	where a.id = 152
+		and a.idx between 1 and 2
+		
+		*/
+	
